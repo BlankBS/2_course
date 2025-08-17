7e3460bb9207f403c7be6b65f0942439bf2df858
BlankBS <sikorskiy.202@mail.ru>
2025-08-17 16:05:25 +0300

    JS lab_4

---
+++ 3_semester/JS/lab_4/js/task_4.js
@@ -0,0 +1,29 @@
+let cache = new WeakMap();
+
+function cashData(item)
+{
+    if(!cache.has(item))
+    {
+        let result = MultipleValue(item);
+        cache.set(item, result);
+        console.log("Данные в кэш успешно добавлены");
+    }
+
+    console.log("Беру данные из кэша");
+    return cache.get(item);
+}
+
+function MultipleValue(item)
+{
+    return item.value * 2;
+}
+
+let itemValue = {value: 29};
+
+let result1 = cashData(itemValue);
+console.log(result1);
+
+let result2 = cashData(itemValue);
+console.log(result2);
+
+itemValue = null;
