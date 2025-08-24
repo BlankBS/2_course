062e952c9645a859819f1cac7c23e87cc1a9f67d
BlankBS <sikorskiy.202@mail.ru>
2025-08-24 16:37:00 +0300

    AiSD lab_1

---
+++ 3_semester/AiSD/lab_1/lab_1/lab_1/lab_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+void SolveTower(int N, int k, int i)
+{
+	if (N == 1)
+	{
+		std::cout << "Переместить диск 1 с " << i << " на " << k << " стержень\n";
+		return;
+	}
+
+	int tmp = 6 - i - k;
+	SolveTower(N - 1, tmp, i);
+	std::cout << "Переместить диск " << N << " с " << i << " на " << k << " стержень\n";
+	SolveTower(N - 1, k, tmp);
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+
+	int N, k;
+
+	std::cout << "Колиество дисков: ";
+	std::cin >> N;
+
+	std::cout << "Конечный стержень: ";
+	std::cin >> k;
+
+	if (N < 1 || k < 2 || k > 3)
+	{
+		std::cout << "Введены некорректные данные";
+	}
+	else
+	{
+		SolveTower(N, k, 1);
+	}
+
+	return 0;
+}
