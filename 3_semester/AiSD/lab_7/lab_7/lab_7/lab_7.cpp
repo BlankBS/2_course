0db981a9bf876430b2a7a24009d95a5babb254ab
BlankBS <sikorskiy.202@mail.ru>
2025-08-28 19:52:03 +0300

    AiSD lab_7

---
+++ 3_semester/AiSD/lab_7/lab_7/lab_7/lab_7.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+
+	std::cout << "Длина последовательности: ";
+	int N;
+	std::cin >> N;
+
+	if (N < 1)
+	{
+		std::cout << "Длина должна быть натуральным числом";
+		return 0;
+	}
+
+	std::vector<int> elements(N);
+	std::vector<int> buff(N);
+	std::vector<int> path(N);
+
+	std::cout << "Элементы последовательности: ";
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> elements[i];
+		buff[i] = 1;
+		path[i] = -1;
+	}
+
+	std::cout << '\n';
+
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (elements[i] > elements[j] &&
+				elements[i] - elements[j] == 1)
+			{
+				if (buff[i] <= buff[j])
+				{
+					buff[i] = buff[j] + 1;
+					path[i] = j;
+				}
+			}
+		}
+	}
+
+	int max = 0;
+	int check = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (buff[i] > max)
+		{
+			check = i;
+			max = buff[i];
+		}
+	}
+
+	std::cout << "Длина максимальной подпоследовательности: " << max << '\n';
+	std::cout << "Элементы максимальной подпоследовательности: ";
+	std::vector<int> result(max + 1); int x;
+	for (int i = max; i > 0; i--)
+	{
+		//std::cout << "\n check = " << check;
+		result[i] = elements[check];
+		x = check;
+		check = path[x];
+	}
+	//std::cout << result.size();
+
+	for (int i = 1; i < max + 1; i++)
+	{
+		std::cout << result[i] << ' ';
+	}
+
+	return 0;
+}
